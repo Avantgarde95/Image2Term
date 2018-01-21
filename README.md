23ee6fc683c45cf185823552b6e91a2456db0cf5
Avantgarde95 <95phm@kaist.ac.kr>
2018-01-21 11:08:12 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Image2Term
+
+Simple script which draws an image on a terminal which supports 256 colors.
+
+### Requirements
+- Python 3.X (Not tested on 2.X)
+- Terminal which supports 256 colors (ex. Mintty, GNOME Terminal, etc.)
+
+### Dependencies
+- [PIL](https://pypi.python.org/pypi/PIL) or [Pillow](https://pillow.readthedocs.io/en/latest/)
+
+### Screenshots
+
+![Mona lisa](https://i.imgur.com/hH4D8kF.png)
+
+![Moon](https://i.imgur.com/deaR5Nh.png)
+
---
+++ image2term.py
@@ -0,0 +1,60 @@
+import sys
+from PIL import Image
+
+class Image2Term(object):
+    def __init__(self, path):
+        self.image = Image.open(path).convert('RGB')
+        self.width, self.height = self.image.size
+
+    def generate(self, height_new, message):
+        # resize the image
+        width_new = int((self.width * height_new) / self.height)
+        image_new = self.image.resize((width_new, height_new), Image.ANTIALIAS)
+        pixels_new = image_new.load()
+
+        # generate ASCII escape codes
+        len_message = len(message)
+        codes = [[''] * width_new for i in range(height_new)]
+
+        for i in range(height_new):
+            for j in range(width_new):
+                r, g, b = pixels_new[j, i]
+                index_message = (j % int(len_message / 2)) * 2
+
+                codes[i][j] = '\033[38;2;%d;%d;%dm%s%s' % (
+                        r, g, b,
+                        message[index_message], message[index_message + 1]
+                )
+
+        # pack the codes
+        result = '\n'.join(
+            ''.join(codes[i][j] for j in range(width_new))
+            for i in range(height_new)
+        )
+
+        # restore the original settings
+        result += '\033[0m'
+
+        return result
+
+def main():
+    if len(sys.argv) < 4:
+        print(
+                'Usage: %s path height message\n' \
+                'ex. %s MonaLisa.png 40 MONALISA\n' \
+                '- path: Path of your image\n'
+                '- height: Height of the result (in characters)\n'
+                '- message: String which will be printed on the result'
+                % (sys.argv[0], sys.argv[0])
+        )
+
+        return
+
+    path = sys.argv[1]
+    height = int(sys.argv[2])
+    message = sys.argv[3]
+
+    print(Image2Term(path).generate(height, message))
+
+main()
+
